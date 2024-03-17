06e0144342ad4bfe01493273f38d66bf653a6f8b
cwilson58 <wilsoncameron42@gmail.com>
2024-03-17 17:24:42 -0600

    move to different VR toolkit

--- ScaryMazeVR/world.gd
+++ ScaryMazeVR/world.gd
@@ -1,9 +1,16 @@
 extends Node3D
 
-var interface : XRInterface
+var xr_interface: XRInterface
 
 func _ready():
-	interface = XRServer.find_interface("OpenXR")
-	if interface and interface.is_initialized():
-		print("We have VR")
+	xr_interface = XRServer.find_interface("OpenXR")
+	if xr_interface and xr_interface.is_initialized():
+		print("OpenXR initialized successfully")
+
+		# Turn off v-sync!
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+
+		# Change our main viewport to output to the HMD
 		get_viewport().use_xr = true
+	else:
+		print("OpenXR not initialized, please check if your headset is connected")
