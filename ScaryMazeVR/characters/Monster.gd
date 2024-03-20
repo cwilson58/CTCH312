08d456bd4fb6fad932a516e05af8fca03d02ba85
cwilson58 <wilsoncameron42@gmail.com>
2024-03-19 22:22:37 -0600

    Break player into scene, add monster

---
+++ ScaryMazeVR/characters/Monster.gd
@@ -0,0 +1,32 @@
+## Class for monsters that can be caught in game
+## Audio clip: https://pixabay.com/sound-effects/dragon-roar-96996/
+class_name Monster extends CharacterBody3D
+
+@onready var light = $SpotLight3D
+var max_distance_squared
+var max_angle
+
+func _ready():
+	max_distance_squared = light.spot_range*light.spot_range
+	max_angle = deg_to_rad(light.spot_angle)
+
+func _process(_delta):
+	light.light_color = Color.WHITE
+	var my_pos : Vector3 = global_transform.origin
+	for body : Node3D in get_tree().get_nodes_in_group("monster_visible"):
+		var body_pos : Vector3 = body.global_transform.origin
+		
+		if my_pos.distance_squared_to(body_pos) > max_distance_squared:
+			continue
+		
+		var monster_facing = -global_transform.basis.z
+		
+		var monster_to_body = my_pos.direction_to(body_pos)
+		var monster_to_body_norm = monster_to_body.normalized()
+		
+		var cos_angle = monster_to_body_norm.dot(monster_facing)
+		var angle = acos(cos_angle)
+		if angle < max_angle:
+			light.light_color = Color.RED
+			if !$Roar.playing:
+				$Roar.play()
